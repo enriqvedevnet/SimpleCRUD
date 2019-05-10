03e468a6b8ac82021aaddba1bb79873015657060
Enrique Quinto <enriquequinto.devnet@gmail.com>
2019-05-09 20:52:38 -0500

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado} from './modelos/empleado';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Crud';
+
+  empleadoArray: Empleado[] = [
+   {id: 1, nombre: 'Luis', pais: 'Peru'},
+   {id: 2, nombre: 'Jose', pais: 'Chile'},
+   {id: 3, nombre: 'Chicho', pais: 'Brasil'}
+  ];
+
+  seleccionarEmpleado: Empleado = new Empleado();
+  OpenForEdit(empleado: Empleado){
+    this.seleccionarEmpleado = empleado;
+  }
+
+  addOrEdit(){
+    if(this.seleccionarEmpleado.id === 0){
+    this.seleccionarEmpleado.id = this.empleadoArray.length + 1;
+    this.empleadoArray.push(this.seleccionarEmpleado);
+    }
+
+    this.seleccionarEmpleado = new Empleado();
+  }
+  delete() {
+    if(confirm('Estas seguro de eliminarlo')){
+      this.empleadoArray = this.empleadoArray.filter(x => x !== this.seleccionarEmpleado);
+      this.seleccionarEmpleado = new Empleado();
+    }
+  }
+
 }
---
+++ src/app/modelos/empleado.ts
@@ -0,0 +1,6 @@
+export class Empleado {
+id: number = 0;
+nombre: string;
+pais: string;
+}
+
